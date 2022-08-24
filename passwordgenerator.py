8207bd4c9b77ace28dd58045e2c8b6eb40b4790d
ALkonrad <kaunelundberg@gmail.com>
2022-08-24 14:16:25 +0200

    Added some basic code

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,1 +1,15 @@
+import random
 
+symbols = 'abcdefghijklmnopqrstuvwxyz'
+symbols += symbols.upper()
+symbols += '0123456789'
+
+password = ''
+
+for full_password in range(3):
+    for password_chunk in range(5):
+        password += symbols[random.randint(0, len(symbols) - 1)]
+    password += '-'
+
+password = password.rstrip('-')
+print(password)
